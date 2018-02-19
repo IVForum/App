42dd69d63716326ebef93debe64360909af0ad0a
Cristian Moraru <cristian.moraru@live.com>
2018-02-19 17:30:27 +0100

    Add project files.

---
+++ IVForum.App/IVForum.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IVForum.App.Models;
+
+namespace IVForum.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
